9b8328c778cdf29452eff7a2c94c93287e40c213
杨超 <yangchao294@126.com>
2013-07-24 23:45:28 +0800

    add scanbarcode libaray

--- O2O/src/com/m6/o2o/DeliverActivity.java
+++ O2O/src/com/m6/o2o/DeliverActivity.java
@@ -1,8 +1,13 @@
 package com.m6.o2o;
 
-import android.os.Bundle;
+import com.google.zxing.client.android.CaptureActivity;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class DeliverActivity extends Activity {
 
@@ -10,13 +15,21 @@ public class DeliverActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_deliver);
+		
+		findViewById(R.id.scan).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent openCameraIntent = new Intent(DeliverActivity.this, CaptureActivity.class);  
+                startActivityForResult(openCameraIntent, 0);
+			}
+		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.delivery, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == RESULT_OK) {
+			((TextView) findViewById(R.id.result)).setText(data.getExtras().getString("result"));
+		}
 	}
-
 }
